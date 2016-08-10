8e8f456a79dbcaf94e731bf48d41a0cb985ab24b
Pascal Fritzen <pascal.fritzen91@gmail.com>
2016-08-10 22:47:50 +0200

    Start refactoring.

--- drones-elective/image_recognition.py
+++ drones-elective/image_recognition.py
@@ -3,9 +3,11 @@ import logging
 import time
 
 import cv2
-import libardrone.libardrone as libardrone
 import numpy as np
 from libardrone.libardrone import at_pcmd
+
+import constants
+from drone_wrapper import ARDroneWrapper
 
 
 def main():
@@ -16,9 +18,6 @@ def main():
     K_D = 0.6
     K_I = 0.02
 
-    DEFAULT_SPEED = 0.1
-
-    #
     X_D = 1000
 
     running = True
@@ -34,26 +33,19 @@ def main():
     e_int = 0
 
     # Init & Setup drone
-    drone = libardrone.ARDrone2()
-    drone.reset()
-    drone.set_speed(DEFAULT_SPEED)
-
-    # bounding_rect_start, bounding_rect_end = ((160, 320), (160, 320))
+    drone = ARDroneWrapper()
+    drone.setup()
 
     try:
         while running:
             drone_camera_image = drone.get_image()
 
-            drone_camera_image_as_rgb = cv2.cvtColor(drone_camera_image.copy(), cv2.COLOR_BGR2RGB)
+            drone_camera_image_as_rgb = cv2.cvtColor(drone_camera_image, cv2.COLOR_BGR2RGB)
 
+            # TODO: DO IN OWN CLASS
             # Find hat in camera view
-            found_hat, hat_bounding_box = find_hat(drone_camera_image.copy())
-
-            center_hat = None
-
-            if found_hat:
-                x, y, w, h = hat_bounding_box
-                center_hat = (x + (w / 2), (y + h / 2))
+            found_object, object_bounding_box, object_center = find_object(drone_camera_image.copy())
+            if found_object:
                 last_hat_time = current_millis()
 
             # Key Press Events
@@ -86,17 +78,27 @@ def main():
                 elif very_last_pressed_key == ord('d'):  # D
                     drone.move_right()
 
+                elif very_last_pressed_key == ord('v'):  # V
+                    should_hold_altitude = True
+                elif very_last_pressed_key == ord('n'):  # N
+                    should_hold_altitude = False
+
+                elif very_last_pressed_key == ord('r'):  # R
+                    drone.reset()
+
+                elif very_last_pressed_key == ord('h'):  # H
+                    drone.hover()
+
+                elif very_last_pressed_key == ord('i'):  # I
+                    save_snapshot(drone_camera_image_as_rgb)
+
                 elif very_last_pressed_key == 0:  # Key Up
-                    # drone.move_up()
                     K_P += 0.01
                 elif very_last_pressed_key == 1:  # Key Down
-                    # drone.move_down()
                     K_P -= 0.01
                 elif very_last_pressed_key == 2:  # Key Left
-                    # drone.turn_left()
                     K_D -= 0.01
                 elif very_last_pressed_key == 3:  # Key Right
-                    # drone.turn_right()
                     K_D += 0.01
 
                 elif very_last_pressed_key == 32:  # Space
@@ -105,39 +107,10 @@ def main():
                     e_int = 0
                     drone.takeoff()
 
-                elif very_last_pressed_key == ord('i'):
-                    save_snapshot(drone_camera_image_as_rgb)
-
-                elif very_last_pressed_key == ord('v'):
-                    should_hold_altitude = True
-
-                elif very_last_pressed_key == ord('n'):
-                    should_hold_altitude = False
-
-                elif very_last_pressed_key == ord('r'):
-                    drone.reset()
-
-                elif very_last_pressed_key == ord('h'):
-                    drone.hover()
-
                 else:
                     print very_last_pressed_key
 
-            nav_data = drone.get_navdata()
-            nav_data = nav_data[0]
-
-            ctrl_state = nav_data['ctrl_state']
-            phi = nav_data['phi']
-            psi = nav_data['psi']
-            theta = nav_data['theta']
-            altitude = nav_data['altitude']
-            vx = nav_data['vx']
-            vy = nav_data['vy']
-            vz = nav_data['vz']
-            battery = nav_data['battery']
-            num_frames = nav_data['num_frames']
-
-            alt.append(altitude)
+            alt.append(drone.altitude)
             ts_alt.append(current_millis() / 1000.0)
 
             if len(alt) > 2:
@@ -145,49 +118,37 @@ def main():
                 ts_alt.pop(0)
 
                 vz = float(alt[1] - alt[0]) / float(ts_alt[1] - ts_alt[0])
-                # print alt, ts_alt, ts_alt[1] - ts_alt[0], vz
+            else:
+                vz = 0.0
 
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            font_size = 0.5
+            show_on_image(drone_camera_image_as_rgb, drone.ctrl_state, drone.phi, drone.psi, drone.theta, drone.altitude, drone.vx, drone.vy, drone.vz, drone.battery, drone.num_frames)
 
-            cv2.putText(drone_camera_image_as_rgb, 'State: %s' % ctrl_state, (5, 15), font, font_size, (255, 255, 255))
+            show_text(drone_camera_image_as_rgb, 'Command: %s' % (very_last_pressed_key if very_last_pressed_key else '-',), (5, 135))
+            show_text(drone_camera_image_as_rgb, 'Should hold altitude: %s' % should_hold_altitude, (5, 150))
 
-            cv2.putText(drone_camera_image_as_rgb, 'Phi (+Roll Right/- Roll Left): %.0f' % phi, (5, 30), font, font_size, (255, 255, 255))
-            cv2.putText(drone_camera_image_as_rgb, 'Psi (+Turn Right/- Turn Left): %.0f' % psi, (5, 45), font, font_size, (255, 255, 255))
-            cv2.putText(drone_camera_image_as_rgb, 'Theta (+Up/-Down): %.0f' % theta, (5, 60), font, font_size, (255, 255, 255))
-
-            cv2.putText(drone_camera_image_as_rgb, 'Altitude: %.0f' % altitude, (5, 75), font, font_size, (255, 255, 255))
-            cv2.putText(drone_camera_image_as_rgb, 'vx/vy/vz: %.3f/%.3f/%.3f' % (vx, vy, vz), (5, 90), font, font_size, (255, 255, 255))
-
-            cv2.putText(drone_camera_image_as_rgb, 'Battery: %.0f%%' % battery, (5, 105), font, font_size, (255, 255, 255) if battery > 20 else (0, 0, 255))
-
-            cv2.putText(drone_camera_image_as_rgb, '# Frames (?): %.0f' % num_frames, (5, 120), font, font_size, (255, 255, 255))
-            cv2.putText(drone_camera_image_as_rgb, 'Command: %s' % (very_last_pressed_key if very_last_pressed_key else '-',), (5, 135), font, font_size, (255, 255, 255))
-            cv2.putText(drone_camera_image_as_rgb, 'Should hold altitude: %s' % should_hold_altitude, (5, 150), font, font_size, (255, 255, 255))
-
-            if center_hat:
+            if object_center:
                 # draw point in viewport
-                cv2.rectangle(drone_camera_image_as_rgb, center_hat, center_hat, (255, 0, 0), 3, 0)
-                delta_z = H / 2 - center_hat[1]
-                if abs(delta_z) > 25:
-                    X_D = altitude + delta_z * 5
-            elif (current_millis() - last_hat_time) > 10000:
-                X_D = 300
+                cv2.circle(drone_camera_image_as_rgb, object_center, 5.0, (255, 0, 0), 3, 0)
 
-            # cv2.rectangle(image_real_shit, (x, y), (x + w, y + h), (200, 0, 0), 2)
+                object_center_delta_z = H / 2 - object_center[1]
+
+                if abs(object_center_delta_z) > 25:
+                    X_D = drone.altitude + object_center_delta_z * 5  # TODO: make factor 5 dependent on distance / size of entire
+
+            elif (current_millis() - last_hat_time) > 10000:
+                X_D = constants.DRONE_DEFAULT_ALTITUDE
 
             X_D = max(300, X_D)
-            e_int += (X_D - altitude) * (ts_alt[1] - ts_alt[0])
+            e_int += (X_D - drone.altitude) * (ts_alt[1] - ts_alt[0])
             e_int = max(e_int, 200)
-            u = K_P * (X_D - altitude) + K_D * (0 - vz) + K_I * e_int
+            u = K_P * (X_D - drone.altitude) + K_D * (0 - vz) + K_I * e_int
 
-            print altitude, X_D, K_P, K_D, u
+            print drone.altitude, X_D, K_P, K_D, u
 
             if should_hold_altitude and key_up:
                 apply_z_velocity(drone, u, vz)  # max(min(1.0, u / 1000.0), -1.0))
 
-            cv2.rectangle(drone_camera_image_as_rgb, (W / 2 - 25, H / 2 - 25), (W / 2 + 25, H / 2 + 25), (0, 255, 0), 2, )
-            # cv2.rectangle(image_real_shit, bounding_rect_start, bounding_rect_end, (0, 255, 0), 2, )
+            cv2.circle(drone_camera_image_as_rgb, (W / 2, H / 2), 25, (0, 255, 0), 2, )
 
             cv2.imshow('Live Drone Cam (Jeronimo)', drone_camera_image_as_rgb)
 
@@ -203,13 +164,33 @@ def main():
     print("Shut down.")
 
 
+def show_on_image(image, ctrl_state, phi, psi, theta, altitude, vx, vy, vz, battery, num_frames):
+    show_text(image, 'State: %s' % ctrl_state, (5, 15))
+
+    cv2.putText(image, 'Phi (+Roll Right/- Roll Left): %.0f' % phi, (5, 30), constants.FONT, constants.FONT_SIZE, (255, 255, 255))
+    cv2.putText(image, 'Psi (+Turn Right/- Turn Left): %.0f' % psi, (5, 45), constants.FONT, constants.FONT_SIZE, (255, 255, 255))
+    cv2.putText(image, 'Theta (+Up/-Down): %.0f' % theta, (5, 60), constants.FONT, constants.FONT_SIZE, (255, 255, 255))
+
+    cv2.putText(image, 'Altitude: %.0f' % altitude, (5, 75), constants.FONT, constants.FONT_SIZE, (255, 255, 255))
+    cv2.putText(image, 'vx/vy/vz: %.3f/%.3f/%.3f' % (vx, vy, vz), (5, 90), constants.FONT, constants.FONT_SIZE, (255, 255, 255))
+
+    cv2.putText(image, 'Battery: %.0f%%' % battery, (5, 105), constants.FONT, constants.FONT_SIZE, (255, 255, 255) if battery > 20 else (0, 0, 255))
+
+    cv2.putText(image, '# Frames (?): %.0f' % num_frames, (5, 120), constants.FONT, constants.FONT_SIZE, (255, 255, 255))
+
+
+def show_text(image, text, pt, font=constants.FONT, font_size=constants.FONT_SIZE, color=(255, 255, 255)):
+    cv2.putText(image, text, pt, font, font_size, color)
+
+
 def apply_z_velocity(drone, u, vz):
     drone.at(at_pcmd, True, 0, 0, min(max(u / 100.0, -1.0), 1.0), 0)
 
 
-def find_hat(image):
+def find_object(image):
     found_hat = False
     hat_bounding_box = None
+    hat_center = None
 
     # Convert to HSV in order to filter by color
     image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
@@ -243,8 +224,9 @@ def find_hat(image):
         if w > 40 and h > 40:
             found_hat = True
             hat_bounding_box = cv2.boundingRect(biggest_contour)
+            hat_center = (x + (w / 2), (y + (h / 2)))
 
-    return found_hat, hat_bounding_box
+    return found_hat, hat_bounding_box, hat_center
 
 
 def save_snapshot(image):
